1eabadc47821ed291897492d8f9c2dd7d58dc616
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2022-05-05 22:23:16 -0400

    sample app

---
+++ app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import json
+import glob
+
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        print("Received request from ", self.client_address[0])
+        content = "Hello there!"
+        self.send_header("Content-Type", "text/plain")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content.encode("utf-8"))
+    
+    def do_HEAD(self):
+        self._set_headers()
+
+
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+if len(argv) == 2:
+    run(port=int(argv[1]))
+else:
+    run()
